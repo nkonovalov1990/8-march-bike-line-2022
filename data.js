9afae4bce46ee755c5077499749c0539bfde7ea5
nkonovalov1990 <n.konovalov1990@gmail.com>
2022-05-11 20:37:33 +0500

    1

--- data.js
+++ data.js
@@ -1,15 +1,15 @@
 const PLANS = [
   {
     title: 'Предлагаемые изменения',
-    map: 'with-bike-line.plan.png',
-    legend: 'functional-zones.legend.png',
+    map: 'with-bike-line.plan.jpg',
+    legend: 'functional-zones.legend.jpg',
     // pinned: true,
     default: true
   },
   {
     title: 'Исходный план',
-    map: 'current.plan.png',
-    legend: 'functional-zones.legend.png',
+    map: 'current.plan.jpg',
+    legend: 'functional-zones.legend.jpg',
     // pinned: true,
     old: true
   }
